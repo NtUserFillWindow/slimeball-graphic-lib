c871ccc9034f5daa6a10ddebb24e1cd32cc94f58
wzxTheSlimeball <2990277260@qq.com>
2026-02-08 17:58:14 +0800

    26/02/08 added license(sorry for my bad memory)

--- include/Painter.hpp
+++ include/Painter.hpp
@@ -1,3 +1,6 @@
+//MIT License
+
+//Copyright (c) 2026 wzxTheSlimeball
 #ifndef PAINTER_HPP
 #define PAINTER_HPP
 
--- src/painter/Painter.cpp
+++ src/painter/Painter.cpp
@@ -1,3 +1,6 @@
+//MIT License
+
+//Copyright (c) 2026 wzxTheSlimeball
 #include "Painter.hpp"
 #include "Logger.hpp"
 #include "Color.hpp"
